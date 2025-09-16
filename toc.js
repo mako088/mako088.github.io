d8d4fdb081be224284da391c8543bb103312bb2f
saylesss88 <saylesss87@proton.me>
2025-09-16 17:39:43 -0400

    Edit Apps that don't work section'

--- toc.js
+++ toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="intro/introduction.html">Introduction</a></li><li class="chapter-item expanded "><a href="android/RethinkDNS_Guide.html"><strong aria-hidden="true">1.</strong> RethinkDNS</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="intro/introduction.html">Introduction</a></li><li class="chapter-item expanded "><a href="android/hardening_android.html"><strong aria-hidden="true">1.</strong> Hardening Android</a></li><li class="chapter-item expanded "><a href="android/open_source_stores.html"><strong aria-hidden="true">2.</strong> Open-Source App Stores</a></li><li class="chapter-item expanded "><a href="android/RethinkDNS_Guide.html"><strong aria-hidden="true">3.</strong> RethinkDNS</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0].split("?")[0];
         if (current_page.endsWith("/")) {
