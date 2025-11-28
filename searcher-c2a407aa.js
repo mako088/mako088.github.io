f735863f6158252be40535380a80e8587b0fb376
saylesss88 <saylesss87@proton.me>
2025-11-28 13:14:17 -0500

    kanagawa-dark

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-30e8be65.js',
+                path_to_root + 'searchindex-b3160df8.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
