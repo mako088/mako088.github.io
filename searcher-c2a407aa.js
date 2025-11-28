f3ee9118d991b2a1cdc9e22671d69d99afb8d545
saylesss88 <saylesss87@proton.me>
2025-11-28 16:34:19 -0500

    ensure an empty index.md

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e9ef3d8d.js',
+                path_to_root + 'searchindex-28776bed.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
