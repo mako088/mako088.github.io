9728d402ab2aa7528b5ee9b244a7a9b7765c35b8
saylesss88 <saylesss87@proton.me>
2025-12-10 10:57:34 -0500

    nix-repl

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-facc858c.js',
+                path_to_root + 'searchindex-117f3aea.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
