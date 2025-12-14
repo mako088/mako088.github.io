22311c9c851a9ba7a6e4bc3e6e34b6b8029d38c0
saylesss88 <saylesss87@proton.me>
2025-12-14 14:41:27 -0500

    testing footer in theme

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0f312f2a.js',
+                path_to_root + 'searchindex-1db9a788.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
