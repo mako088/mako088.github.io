bb56d08a073e202fb4b5a8d9d2532f2933aeeb1a
saylesss88 <saylesss87@proton.me>
2025-12-24 16:58:44 -0500

    RethinkDNS

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-23d1c973.js',
+                path_to_root + 'searchindex-c6952964.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
